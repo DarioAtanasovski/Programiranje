7615d78cc8b6954de16446631b139d0a6eec5355
DarioAtanasovski <atanasovskidaqi@gmail.com>
2026-02-16 10:37:29 +0100

    Refactor Knight's Tour algorithm implementation

--- Knight walk/main.cpp
+++ Knight walk/main.cpp
@@ -2,76 +2,103 @@
 #include <vector>
 using namespace std;
 
-int ABS(int x) {
-    if (x < 0) return -x;
-    return x;
+const int N = 8;
+int board[N][N];
+
+int dr[8] = {-2,-2,-1,-1, 1, 1, 2, 2};
+int dc[8] = {-1, 1,-2, 2,-2, 2,-1, 1};
+
+struct Cell {
+    int r, c;
+};
+
+bool inside(int r, int c) {
+    return r >= 0 && r < N && c >= 0 && c < N;
 }
 
-void swapInt(int &a, int &b) {
-    int t = a; a = b; b = t;
-}
-
-void reversePart(vector<int>& a, int l, int r) {
-    while (l < r) {
-        swapInt(a[l], a[r]);
-        l++; r--;
+int onwardDegree(int r, int c) {
+    int cnt = 0;
+    for (int k = 0; k < 8; k++) {
+        int nr = r + dr[k], nc = c + dc[k];
+        if (inside(nr, nc) && board[nr][nc] == 0) cnt++;
     }
-}
-
-bool nextPermutation(vector<int>& a) {
-    int n = (int)a.size();
-
-
-    int i = n - 2;
-    while (i >= 0 && a[i] >= a[i + 1]) i--;
-    if (i < 0) return false;
-
-
-    int j = n - 1;
-    while (a[j] <= a[i]) j--;
-
-
-    swapInt(a[i], a[j]);
-
-
-    reversePart(a, i + 1, n - 1);
-
-    return true;
-}
-
-long long countNQueensBruteForce(int n) {
-    vector<int> p(n);
-
-
-    for (int i = 0; i < n; i++) p[i] = i;
-
-    long long cnt = 0;
-
-    while (true) {
-        bool ok = true;
-
-
-        for (int i = 0; i < n && ok; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (ABS(i - j) == ABS(p[i] - p[j])) {
-                    ok = false;
-                    break;
-                }
-            }
-        }
-
-        if (ok) cnt++;
-
-
-        if (!nextPermutation(p)) break;
-    }
-
     return cnt;
 }
 
+void sortCandidates(vector<Cell>& cand) {
+    int m = (int)cand.size();
+    for (int i = 0; i < m; i++) {
+        int best = i;
+        int bestDeg = onwardDegree(cand[i].r, cand[i].c);
+
+        for (int j = i + 1; j < m; j++) {
+            int d = onwardDegree(cand[j].r, cand[j].c);
+            if (d < bestDeg) {
+                bestDeg = d;
+                best = j;
+            }
+        }
+        if (best != i) {
+            Cell tmp = cand[i];
+            cand[i] = cand[best];
+            cand[best] = tmp;
+        }
+    }
+}
+
+bool dfs(int r, int c, int step) {
+    board[r][c] = step;
+    if (step == N * N) return true;
+
+    vector<Cell> cand;
+    for (int k = 0; k < 8; k++) {
+        int nr = r + dr[k], nc = c + dc[k];
+        if (inside(nr, nc) && board[nr][nc] == 0) {
+            cand.push_back(Cell{nr, nc});
+        }
+    }
+
+    sortCandidates(cand);
+
+    for (int i = 0; i < (int)cand.size(); i++) {
+        int nr = cand[i].r, nc = cand[i].c;
+        if (dfs(nr, nc, step + 1)) return true;
+    }
+
+    board[r][c] = 0;
+    return false;
+}
+
 int main() {
-    int n;
-    cin >> n;
-    cout << countNQueensBruteForce(n) << "\n";
+    int row, col; 
+    cin >> row >> col;
+
+  
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            board[i][j] = 0;
+
+    int sr = row - 1, sc = col - 1;
+    if (!inside(sr, sc)) {
+        cout << "Nevalidni koordinati.\n";
+        return 0;
+    }
+
+    if (!dfs(sr, sc, 1)) {
+        cout << "Nema najdeno tura.\n";
+        return 0;
+    }
+
+   
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int v = board[i][j];
+            if (v < 10) cout << ' ';   
+            cout << v;
+            if (j != N - 1) cout << ' ';
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
